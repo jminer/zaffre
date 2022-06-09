a1fac0d6519e6d30a27d927897a57d2dc9a08f8d
Jordan Miner <jminer7@gmail.com>
2022-06-09 01:37:35 -0500

    Begin work on the TextAnalyzer API

--- src/text_analyzer.rs
+++ src/text_analyzer.rs
@@ -1,3 +1,27 @@
+use std::ops::Range;
+
+
+#[derive(Debug, Clone, Copy)]
+pub enum TextDirection {
+    LeftToRight,
+    RightToLeft,
+}
+
+#[derive(Debug)]
+pub struct TextAnalyzerRun {
+    text_range: Range<usize>,
+    direction: TextDirection,
+}
+
+impl TextAnalyzerRun {
+    pub fn text_range(&self) -> Range<usize> {
+        return self.text_range.clone();
+    }
+
+    pub fn direction(&self) -> TextDirection {
+        return self.direction;
+    }
+}
 
 pub struct TextAnalyzer {
     text: String,
@@ -9,4 +33,17 @@ impl TextAnalyzer {
     // as well as shaping and glyph placement?
 
     // have all fn take &self, not &mut self
+
+    pub fn new(text: String) -> Self {
+        todo!()
+    }
+
+    pub fn get_runs(&self) -> Vec<TextAnalyzerRun> {
+
+        todo!()
+    }
+
+    pub fn get_glyphs_and_positions(&self, text_range: Range<usize>, run: TextAnalyzerRun) {
+
+    }
 }
